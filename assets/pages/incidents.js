55911be76b41ccb9cb14ee39c48deeaacf49195d
Minho Kim <music4537@naver.com>
2021-10-02 21:30:23 -0500

    layouts

--- assets/pages/incidents.js
+++ assets/pages/incidents.js
@@ -8,29 +8,29 @@ export default function Incidents(props){
     return (
         <View style={{ flex: 1 }}>
             <View style={{ flex: 0.15 }}><Text></Text></View>
-            <View style={styles.workername}><Text >Created by(worker name)</Text></View>
-            <View style={styles.equipmentname}><Text>Equipmet name</Text></View>
-            <View style={styles.safetydocs}><Text>Safty docs</Text></View>
-            <View style={styles.incidentsinfo}><Text>Describe the incidents</Text></View>
+            <View style={styles.workername}><Text style={{fontSize: 18, marginTop: 10}} >Created by (worker name)</Text></View>
+            <View style={styles.equipmentname}><Text style={{fontSize: 18, marginTop: 10}}>Equipmet name</Text></View>
+            <View style={styles.safetydocs}><Text style={{fontSize: 18, marginTop: 10}}>Safty docs</Text></View>
+            <View style={styles.incidentsinfo}><Text style={{fontSize: 18, marginTop: 10}}>Describe the incidents</Text></View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     workername: {
-        flex: 1,
+        flex: 0.5,
         backgroundColor: "#E6E6FA",
         fontSize: 60,
         alignItems: "center",
     },
     equipmentname: {
-        flex: 1,
+        flex: 0.5,
         backgroundColor: "#FFF0F5",
         fontSize: 60,
         alignItems: "center"
     },
     safetydocs: {
-        flex: 1,
+        flex: 0.8,
         backgroundColor: "#E6E6FA",
         fontSize: 60,
         alignItems: "center"
