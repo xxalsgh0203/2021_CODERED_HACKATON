b0d15a67da37e568f9d8ff5c36d39976a61ed917
Minho Kim <music4537@naver.com>
2021-10-02 20:50:42 -0500

    8:50

--- assets/pages/incidents.js
+++ assets/pages/incidents.js
@@ -1,14 +1,40 @@
 import * as React from "react";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 export default function Incidents(props){
+
+    // const [workerName, equipmentName, safetyDocs, incidentInfo] = useState("");
+
     return (
         <View style={{ flex: 1 }}>
-            <View style={{flex: 1, backgroundColor: "grey"}}><Text>Incident show page</Text></View>
-            <View style={{flex: 1, backgroundColor: "tomato"}}><Text>Created by(worker name)</Text></View>
-            <View style={{flex: 1, backgroundColor: "grey"}}><Text>Equipmet name</Text></View>
-            <View style={{flex: 1, backgroundColor: "tomato"}}><Text>Safty docs</Text></View>
-            <View style={{flex: 1, backgroundColor: "grey"}}><Text>Describe the incidents</Text></View>
+            <View style={{ flex: 0.15 }}><Text></Text></View>
+            <View style={styles.workername}><Text>Created by(worker name)</Text></View>
+            <View style={styles.equipmentname}><Text>Equipmet name</Text></View>
+            <View style={styles.safetydocs}><Text>Safty docs</Text></View>
+            <View style={styles.incidentsinfo}><Text>Describe the incidents</Text></View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    workername: {
+        flex: 1,
+        backgroundColor: "grey",
+        fontSize: 60
+    },
+    equipmentname: {
+        flex: 1,
+        backgroundColor: "tomato",
+        fontSize: 60
+    },
+    safetydocs: {
+        flex: 1,
+        backgroundColor: "grey",
+        fontSize: 60
+    },
+    incidentsinfo: {
+        flex: 1,
+        backgroundColor: "tomato",
+        fontSize: 60
+    }
+})
